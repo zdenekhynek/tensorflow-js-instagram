428cf4d49462d257bf877d5052b0e90526beace5
Zdenek Hynek <zdenek.hynek@gmail.com>
2019-11-07 17:22:13 +0000

    Justin bieber followers and cleanup test

--- src/test.js
+++ src/test.js
@@ -1,84 +1,15 @@
-console.log('test.js');
-
 import $ from "jquery";
-import dt from 'datatables.net';
+import * as tf from "@tensorflow/tfjs";
 
+// load generic datagrid component
+import dt from 'datatables.net';
 dt();
 
-// import * as tf from "@tensorflow/tfjs";
-// import * as tfvis from "@tensorflow/tfjs-vis";
-// import * as Papa from "papaparse";
-
-
-// const features = ["profile pic", "nums/length username", "fullname words", "nums/length fullname",
-// "name==username", "description length", "external URL", "private", "#posts", "#followers", "#follows"];
-
-
-// Papa.parsePromise = function (file) {
-//   return new Promise(function (complete, error) {
-//     Papa.parse(file, {
-//       header: true,
-//       download: true,
-//       dynamicTyping: true,
-//       complete,
-//       error
-//     });
-//   });
-// };
-
-// const oneHot = outcome => Array.from(tf.oneHot(outcome, 2).dataSync());
-
-// const prepareData = async (url) => {
-//   // const csv = await Papa.parsePromise(
-//   //   "https://raw.githubusercontent.com/curiousily/Logistic-Regression-with-TensorFlow-js/master/src/data/diabetes.csv"
-//   // );
-//   const csv = await Papa.parsePromise(
-//     url
-//   );
-
-//   return csv.data;
-// };
-
-// const createFeatures = (data, features) => {
-//   return data.map(row =>
-//     features.map(feature => {
-//       const val = row[feature];
-//       return val === undefined ? 0 : val;
-//     })
-//   );
-// };
-
-// async function loadPretrainedModel() {
-//   console.log(tf);
-//   const model = await tf.loadLayersModel('./pretrained-model.json');
-
-//   const optimizer = tf.train.adam(0.001);
-//   model.compile({
-//     optimizer: optimizer,
-//     loss: "binaryCrossentropy",
-//     metrics: ["accuracy"]
-//   });
-
-//   return model;
-// }
-
-// async function predictData(model) {
-//   const testData = await prepareData('./tiz.alocci-followers.csv');
-//   //  const testData = await prepareData('./signal_noise-followers.csv');
-//   const testX = tf.tensor(createFeatures(testData, features));
-//   console.log('testData', testData, 'textX', testX);
-//   const testPredictions = model.predict(testX).argMax(-1);
-    
-//   // console.log('testPredictions', testPredictions.dataSync(), 'labels', labels.dataSync());
-//   console.log('copare lengths',  testPredictions.dataSync().length, testData.length);
-  
-//   testPredictions.dataSync().forEach((d, i) => {
-//     console.log(d, testData[i]);
-//   });
-// }
-
-import * as tf from "@tensorflow/tfjs";
 import { loadCsv } from "./utils.js";
+
+//  which data you want predict
+const DATA_PATH = './instagram_test.csv';
+
 
 export async function loadData(url) {
   const data = await loadCsv(url);
@@ -100,6 +31,10 @@ export async function loadPretrainedModel() {
   return model;
 }
 
+
+/**
+ * Returns [[], [], []]
+ */
 export async function prepareData(data, features) {
   console.log("2. Preparing data ...");
   const X = data.map((row) => {
@@ -129,11 +64,6 @@ export function displayResults(data, features) {
   columns.unshift({ title: 'username' });
   columns.unshift({ title: 'fake' });
 
-  // //  amend data
-  // const data = X.map((x, i) => {
-  //   return [y[i], 'username', ...x];
-  // })
-
   $('#results').DataTable({
     'pageLength': 35,
     data,
@@ -142,14 +72,13 @@ export function displayResults(data, features) {
 }
 
 (async() => {
-  const dataPath = './instagram_test.csv';
-
-  //  make sure this is the same with 
+  
+  //  make sure this is the same with index.js
   const features = ["profile pic", "nums/length username", "fullname words", "nums/length fullname",
     "name==username", "description length", "external URL", "private", "#posts", "#followers", "#follows"];
   
   const model = await loadPretrainedModel();
-  const data = await loadData(dataPath);
+  const data = await loadData(DATA_PATH);
   const [X, y] = await predictData(data, model, features);
   
   //  amend data 
